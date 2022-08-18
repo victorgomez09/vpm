db3b6d134c2351ba298d6d5422049065771a0bab
Victor Gomez <39098525+Victin09@users.noreply.github.com>
2022-08-18 16:30:34 +0000

    add cors filter to gateway

--- backend/auth-service/src/main/java/com/vira/vpm/authservice/security/WebSecurityConfig.java
+++ backend/auth-service/src/main/java/com/vira/vpm/authservice/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Component
 public class WebSecurityConfig {
@@ -11,6 +12,7 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     return http.build();
   }
 }
--- backend/gateway/src/main/java/com/vira/vpm/gateway/security/WebClientConfig.java
+++ backend/gateway/src/main/java/com/vira/vpm/gateway/security/WebClientConfig.java
@@ -3,6 +3,8 @@ package com.vira.vpm.gateway.security;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -12,5 +14,12 @@ public class WebClientConfig {
   @LoadBalanced
   public WebClient.Builder builder() {
     return WebClient.builder();
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    return http.build();
   }
 }
